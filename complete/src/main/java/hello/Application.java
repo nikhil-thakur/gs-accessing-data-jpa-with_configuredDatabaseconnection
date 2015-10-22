5a6a94b954a93578e48d584e91e35062cb61b301
nikhil-thakur <nikhil.thakur.java@gmail.com>
2015-10-21 20:47:20 -0400

    Fixed database issues. Project is running.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -18,15 +18,11 @@ public class Application implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
         // save a couple of customers
-        repository.save(new Customer("Jack", "Bauer"));
-        repository.save(new Customer("Chloe", "O'Brian"));
-        repository.save(new Customer("Kim", "Bauer"));
-        repository.save(new Customer("David", "Palmer"));
-        repository.save(new Customer("Michelle", "Dessler"));
+        repository.save(new Customer("Umang", "Lohia"));
+        repository.save(new Customer("Thakur", "Thakur"));
 
         // fetch all customers
         System.out.println("Customers found with findAll():");
-        System.out.println("-------------------------------");
         for (Customer customer : repository.findAll()) {
             System.out.println(customer);
         }
@@ -40,9 +36,9 @@ public class Application implements CommandLineRunner {
         System.out.println();
 
         // fetch customers by last name
-        System.out.println("Customer found with findByLastName('Bauer'):");
+        System.out.println("Customer found with findByLastName('Thakur'):");
         System.out.println("--------------------------------------------");
-        for (Customer bauer : repository.findByLastName("Bauer")) {
+        for (Customer bauer : repository.findByLastName("Thakur")) {
             System.out.println(bauer);
         }
     }
